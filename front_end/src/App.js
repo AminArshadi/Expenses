5fd49208ef960a7dbb20ae5b22e05bb4c19cd69b
Amin Arshadi <aminarshadi@amins-mbp.home>
2024-05-04 16:03:21 -0400

    file updated

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -12,6 +12,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} index />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/home" element={<HomePage />} />
